0f0c1ecc171f754289f161100ac4ed5a19dbf803
Toomas Oosalu <toomas.oosalu@gmail.com>
2021-04-10 00:49:14 +0400

    feat: implement finding the alpha color

---
+++ src/events/process/metadata-alpha.ts
@@ -0,0 +1,14 @@
+import { nth } from 'lodash'
+import { padLeft, toHex } from 'web3-utils'
+
+const hasAlpha = (metadata: Array<string>): boolean => {
+  const data = nth(metadata, 1)
+  if (!data) return false
+
+  const hex = padLeft(toHex(data), 64).slice(2)
+  // last bit is alpha channel flag
+  return hex.slice(hex.length - 1) !== '0'
+}
+
+
+export default (metadata: Array<string>, colorList: Array<number>): number | null => hasAlpha(metadata) ? colorList[0] : null
